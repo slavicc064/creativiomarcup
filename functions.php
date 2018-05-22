187b1fb4de064dfb0b6ae4d6bfccebece25a9ff8
root <slavicc064@gmail.com>
2018-05-22 22:43:08 +0300

    button commit

--- functions.php
+++ functions.php
@@ -3,4 +3,9 @@
 include "classes/ThemeSupport.php";
 include "bootstrap-menu.php";
 include "classes/Editor.php";
+
+function my_theme_add_editor_styles() {
+    add_editor_style( 'editor-styles.css' );
+}
+add_action( 'current_screen', 'my_theme_add_editor_styles' );
 ?>
